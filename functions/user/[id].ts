5a2927b33911966cc35d3f30fb2580d0f163bd78
Bigjango13 <bigjango13@gmail.com>
2024-05-19 19:28:16 -0700

    Make the login page more "secure", properly redirect /user/x pages

--- functions/user/[id].ts
+++ functions/user/[id].ts
@@ -1,5 +1,10 @@
 interface Env { }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
-  return Response.redirect(`/user?id=${context.params.id}`)
+  return new Response(null, {
+    status: 303,
+    headers: {
+      'Location': `/user?id=${context.params.id}`,
+    },
+  })
 }
